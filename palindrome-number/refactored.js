1a35c4adfe4b046eb9ec6f870d1e1159ef44a903
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-03-16 10:15:13 -0500

    updated palindrom-number

--- palindrome-number/refactored.js
+++ palindrome-number/refactored.js
@@ -3,24 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-
-    if (x < 0) {
+    if (x < 0 || x % 10 === 0 && x !== 0) {
         return false
     }
 
-
-
-    if (x % 10 === 0 && x !== 0) {
-        return false
-    }
 
     let revertedNumber = 0;
     while (x > revertedNumber) {
-        console.log(revertedNumber * 10, '+', x % 10)
         revertedNumber = revertedNumber * 10 + x % 10;
-        x = Math.floor(x / 10);
+        x = ~~(x / 10);
     }
-    return Math.floor(x) == revertedNumber || Math.floor(x) == Math.floor(revertedNumber / 10)    // while( total > 0 ){
+    return ~~(x) == revertedNumber || ~~(x) == ~~(revertedNumber / 10)    // while( total > 0 ){
+
 
 
 };
